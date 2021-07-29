9e27013b392c9f8f8db6392044ee8e095b9eb411
pjkong <41837063+pjkong@users.noreply.github.com>
2021-07-30 01:14:10 +0800

    generate client secret key with rb

---
+++ client.rb
@@ -0,0 +1,24 @@
+require 'jwt'
+
+key_file = 'key_file.txt'
+team_id = 'team_id'
+client_id = 'client_id'
+key_id = 'key_id'
+
+ecdsa_key = OpenSSL::PKey::EC.new IO.read key_file
+
+headers = {
+  'kid' => key_id
+}
+
+claims = {
+	'iss' => team_id,
+	'iat' => Time.now.to_i,
+	'exp' => Time.now.to_i + 86400*180,
+	'aud' => 'https://appleid.apple.com',
+	'sub' => client_id,
+}
+
+token = JWT.encode claims, ecdsa_key, 'ES256', headers
+
+puts token
